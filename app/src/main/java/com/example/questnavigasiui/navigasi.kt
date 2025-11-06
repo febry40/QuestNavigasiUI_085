a9a106794dd4bbb91eb32525718310b51adb7e0c
febry40 <febryevayonisa.com>
2025-11-06 22:27:53 +0700

    menambahkan navigasi antar layar formulir dan detail

---
+++ app/src/main/java/com/example/questnavigasiui/navigasi.kt
@@ -0,0 +1,54 @@
+package com.example.questnavigasiui
+
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavController
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.questnavigasiui.view.FormIsian
+import com.example.questnavigasiui.view.TampilData
+
+enum class Navigasi{
+    Formulir,
+    Detail
+}
+
+@Composable
+fun DataApp(
+    navController: NavHostController = rememberNavController(),
+    modifier: Modifier = Modifier
+){
+    Scaffold { isiRuang->
+        NavHost(
+            navController = navController,
+            startDestination = Navigasi.Formulir.name,
+
+            modifier = Modifier.padding(isiRuang)){
+            composable(route = Navigasi.Formulir.name){
+                FormIsian (
+                    //pilihanJK = JenisK.map { id -> konteks.resources.getString}
+                    OnSubmitBtnClick = {
+                        navController.navigate(Navigasi.Detail.name)
+                    }
+                )
+            }
+            composable(route = Navigasi.Detail.name){
+                TampilData(
+                    onBackBtnClick = {
+                        cancelAndBackToFormulir(navController)
+                    }
+                )
+            }
+        }
+    }
+}
+private fun cancelAndBackToFormulir(
+    navController: NavController
+){
+    navController.popBackStack(Navigasi.Formulir.name,
+        inclusive = false)
+}
